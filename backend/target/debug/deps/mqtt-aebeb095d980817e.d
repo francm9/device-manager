f36b6c5dd781ee49a5c0c8b63154e716a552ae82
dev.francm <dev.francm@gmail.com>
2026-03-07 00:07:30 +0100

    remove target folder

--- backend/target/debug/deps/mqtt-aebeb095d980817e.d
+++
@@ -1,5 +0,0 @@
-/home/devfran/projects/device-manager/backend/target/debug/deps/mqtt-aebeb095d980817e.d: mqtt/src/main.rs
-
-/home/devfran/projects/device-manager/backend/target/debug/deps/mqtt-aebeb095d980817e: mqtt/src/main.rs
-
-mqtt/src/main.rs:
--- backend/target/debug/mqtt.d
+++
@@ -1,1 +0,0 @@
-/home/devfran/projects/device-manager/backend/target/debug/mqtt: /home/devfran/projects/device-manager/backend/mqtt/src/main.rs

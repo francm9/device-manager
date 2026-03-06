255df97f43c5617b1f8c0b78e7491926bb49bc7a
dev.francm <dev.francm@gmail.com>
2026-03-07 00:03:05 +0100

    init backend project

---
+++ backend/target/debug/deps/mqtt-aebeb095d980817e.d
@@ -0,0 +1,5 @@
+/home/devfran/projects/device-manager/backend/target/debug/deps/mqtt-aebeb095d980817e.d: mqtt/src/main.rs
+
+/home/devfran/projects/device-manager/backend/target/debug/deps/mqtt-aebeb095d980817e: mqtt/src/main.rs
+
+mqtt/src/main.rs:
---
+++ backend/target/debug/mqtt.d
@@ -0,0 +1,1 @@
+/home/devfran/projects/device-manager/backend/target/debug/mqtt: /home/devfran/projects/device-manager/backend/mqtt/src/main.rs
